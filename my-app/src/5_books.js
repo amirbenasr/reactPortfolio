88040fd8f3c9b190323010959755e591c49ad1ab
roflan94 <amir.bennasr@esprit.tn>
2021-01-14 19:44:35 +0100

    e-book

---
+++ my-app/src/5_books.js
@@ -0,0 +1,9 @@
+export var books_5 = [
+  {
+    title: "nobel",
+  },
+  { title: "nobel" },
+  { title: "les miserbales" },
+];
+
+// export default book;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//importing models
+import { books_5 } from "./5_books";
+//explicit function
+
+//implicit function
+const Title = () => {
+  return <h1>We Were Liars</h1>;
+};
+const Author = () => <h4>E. Lockhart</h4>;
+
+const books = [
+  {
+    title: "we are liars",
+    author: "E. erkhoml",
+  },
+  {
+    title: "el kitsh el 3arabi",
+    author: "Safi said",
+  },
+];
+//normal function
+function Book({ book, children }) {
+  const title = "We Were Liars";
+  const Author = "E. lockhart";
+  const clickEvent = () => {
+    alert(book.title);
+  };
+  const dataHandler = (bookiz) => {
+    alert(bookiz.title);
+  };
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/916kdpY0YRL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1>{book.title}</h1>
+      <h4>{book.author ?? "nothing"}</h4>
+      <button onClick={clickEvent}>click to show title</button>
+      <button onClick={() => dataHandler(book)}>
+        send data to event handler
+      </button>
+      {children}
+    </article>
+  );
+}
+var book = {
+  title: "we are liars",
+  author: "E. lockhart",
+};
+function BookList() {
+  console.log(books_5.length);
+  return (
+    <section className="booklist">
+      {books.map((e, index) => {
+        return <Book key={index} book={e}></Book>;
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<BookList></BookList>, document.getElementById("root"));
+export default Book;
